c5c52d53b1106f5d8fc750918c610f874d99bc10
fatima-dah <dahnini.fatima@hotmail.fr>
2020-11-03 12:16:30 +0100

    Create NavBar, Responve and design

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import Header from './components/Header/Header';
+
 function App() {
-  return <div>coucou</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
 
 export default App;
